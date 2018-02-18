cfb70fa8e92cc7e4836e3957fa0c69886af8bc6f
petars95 <sejic.petar@gmail.com>
2018-02-18 05:38:36 +0100

    mozda nece

---
+++ Mongo Fudbal/Mongo Fudbal/DodajLigu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace Mongo_Fudbal
+{
+    public partial class DodajLigu : Form
+    {
+        public DodajLigu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string naziv = txtNaziv.Text;
+            string drzava = txtDrzava.Text;
+
+            if (String.IsNullOrEmpty(naziv) || String.IsNullOrEmpty(drzava))
+            {
+                return;
+            }
+
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("fudbal");
+
+            var collection = db.GetCollection<Liga>("lige");
+
+            Liga liga = new Liga { Ime = naziv, Drzava = drzava };
+
+
+            collection.Insert(liga);
+
+
+            foreach (Liga lg in collection.FindAll())
+            {
+                MessageBox.Show(lg.Ime);
+            }
+
+            this.Close();
+        }
+    }
+}
---
+++ Mongo Fudbal/Mongo Fudbal/FormLiga.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver.Builders;
+
+namespace Mongo_Fudbal
+{
+    public partial class FormLiga : Form
+    {
+        public string Id { get; set; }
+
+        public FormLiga()
+        {
+            InitializeComponent();
+        }
+
+        public FormLiga(string id)
+        {
+            this.Id = id;
+        }
+
+        private void FormLiga_Load(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("fudbal");
+            var collection = db.GetCollection<Liga>("lige");
+
+            var query = Query.EQ("id", this.Id);
+
+        }
+    }
+}
